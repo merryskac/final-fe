1263c15cc4539098c367256d8a394a6c8377821f
unknown <merryskachr@gmail.com>
2023-08-14 23:01:59 +0800

    edit

--- src/components/videoContent.jsx
+++ src/components/videoContent.jsx
@@ -8,13 +8,6 @@ const VideoContent = ({title, url}) => {
     width='full' pl={{md:'150px',sm: '130px'}}
     mb='px'
     >
-      {/* <video controls style={{
-        margin:'auto',
-        width:'100%',
-        height:'100%'
-      }}>
-      </video> */}
-
         <Box as="iframe" 
         id="ytplayer" type="text/html" width="100%" height="100%"
         src={url}
