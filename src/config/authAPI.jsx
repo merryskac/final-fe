b2d9a37fc1acc39ba1278bf7e5f9cedf53bc32ad
unknown <merryskachr@gmail.com>
2023-08-14 21:53:06 +0800

    edit

--- src/config/authAPI.jsx
+++ src/config/authAPI.jsx
@@ -1,10 +1,18 @@
+import Cookies from "universal-cookie"
+
+const cookies = new Cookies()
+
 export const cekToken = () =>{
   try{
   const cek = fetch('https://final-term-git-main-wheytosharepalu-gmailcom.vercel.app/play/cektoken',{
+    method:'POST',
     credentials:'include',
     headers:{
       authorization: "Bearer "+localStorage.getItem('access_token')
-    }
+    },
+    body:JSON.stringify({
+      refreshToken: cookies.get('refreshToken')}
+    )
   }).then(data=>data.json())
   .then(data=>data)
   return cek
