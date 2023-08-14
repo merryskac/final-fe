adf852d7dac57431d9497f2f63d656ae70b8f3d2
unknown <merryskachr@gmail.com>
2023-08-14 18:09:37 +0800

    netlify

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/frontend/"
-  // build:{
-  //   rollupOptions:{
-  //     external:'@chakra-ui/react'
-  //   }
-  // }
+
 })
 
 
